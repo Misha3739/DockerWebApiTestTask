7f0611e8458e4d9396c158a9ca2095eaf1327bc5
Mikhail.udot <Mikhail.udot@venafi.com>
2019-04-11 22:03:06 +0300

    DockerFile for WebApi1 prepared

--- WebApi1/Program.cs
+++ WebApi1/Program.cs
@@ -19,6 +19,7 @@ namespace WebApi1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:8070")
                 .UseStartup<Startup>();
     }
 }
